a71f3ff8169f54ee75937a453603c22d799c1d8f
Junior Rumiche <juniorrumichem@gmail.com>
2023-04-01 13:36:19 -0500

    fix: button colomode

---
+++ src/components/base/Navbar.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Heading,
+  HStack,
+  IconButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Sidebar } from "./Sidebar";
+
+export const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box>
+            <HStack spacing={3}>
+              <IconButton
+                onClick={onOpen}
+                aria-label="...toogle"
+                icon={<HamburgerIcon fontSize={25} />}
+              />
+              <Heading fontSize={30}>CAS</Heading>
+            </HStack>
+          </Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <IconButton
+                rounded="3xl"
+                fontSize={18}
+                aria-label="..."
+                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                onClick={toggleColorMode}
+              ></IconButton>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar size={"sm"} />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar size={"2xl"} />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Configuracion</MenuItem>
+                  <MenuItem>Cerrar Session</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+
+      <Sidebar onClose={onClose} isOpen={isOpen} />
+    </>
+  );
+};
